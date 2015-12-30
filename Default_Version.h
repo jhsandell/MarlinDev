5a4b358d9b50fc131d03903698f80f05e06283f5
Richard Wackerbarth <rkw@dataplex.net>
2015-12-30 10:26:58 -0600

    Arduino IDE 1.6.7 released
    Since this uses a slightly different platform structure,
    we will drop support for earlier versions of the IDE

--- Default_Version.h
+++ Default_Version.h
@@ -8,7 +8,7 @@
 #if true
 #define SHORT_BUILD_VERSION "1.2.0-dev"
 #define DETAILED_BUILD_VERSION "1.2.0-dev From Archive"
-#define STRING_DISTRIBUTION_DATE "2015-10-00 12:00"
+#define STRING_DISTRIBUTION_DATE "2015-12-30 12:00"
 // It might also be appropriate to define a location where additional information can be found
 #define SOURCE_CODE_URL  "http:// ..."
 #endif
