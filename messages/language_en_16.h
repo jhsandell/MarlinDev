ab022ed0c0c2d822c9939b328ec745c6ad8c1fb2
Richard Wackerbarth <rkw@dataplex.net>
2015-12-31 06:17:11 -0600

    Provide short versions of messages for use on displays with only 16 characters

---
+++ messages/language_en_16.h
@@ -0,0 +1,23 @@
+/**
+ * English 16
+ *
+ * This is for LCDs 16 characters in length
+ * documentation/LCDLanguageFont.md
+ *
+ */
+#ifndef LANGUAGE_EN_16_H
+#define LANGUAGE_EN_16_H
+
+#define MSG_DISABLE_STEPPERS                "Steppers Off"
+#define MSG_SET_HOME_OFFSETS                "SetOffset home"
+#define MSG_SWITCH_PS_ON                    "Power on"
+#define MSG_SWITCH_PS_OFF                   "Power off"
+#define MSG_RESTORE_FAILSAFE                "Load defaults"
+#define MSG_USERWAIT                        "Wait for user"
+#define MSG_POSITION_UNKNOWN                "Home X/Y then Z"
+#define MSG_PREHEAT_PLA                     "Heat PLA"
+#define MSG_PREHEAT_ABS                     "Heat ABS"
+
+// The remainder of the messages will be picked up from "language_en.h" as their default
+
+#endif // LANGUAGE_EN_16_H
